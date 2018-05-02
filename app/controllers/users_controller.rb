e90a9bd33e153d59372c5763809ecb3d36558f7b
YutoAoki <frcsnt@gmail.com>
2018-05-02 14:37:54 +0900

    ユーザー登録実装／フォーム完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user =User.new(name: params[:user][:name])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
 end
